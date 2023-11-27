af5c44a557856f75f00cc09f6d1540450908e46a
Matteo <5095191+mmazzanti@users.noreply.github.com>
2023-11-27 14:52:23 +0100

    change sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@ release = '1.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
